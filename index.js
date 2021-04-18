eed88cd7151c00a783b15f14a28e3d53ddb14feb
Carla Jeannine Cruzada <80302664+enigmaticarly@users.noreply.github.com>
2021-04-18 08:59:03 +0800

    Added database model

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const dotenv = require('dotenv');
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-//const db = require('./models/db.js');
+const db = require('./model/db.js');
+
 
 const app = express() //initializing an express server and passing to app 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +17,7 @@ app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/images', express.static(__dirname + 'public/images'));
 
 
-app.get('/', function(req, res){ // when someone connects to server, get request to this path, and reply back with hello world
+app.get('/', function(req, res){ 
 
     fs.readFile('./views/home.html', function(err, data){{
         res.setHeader('Content-Type', 'text/html');
@@ -32,7 +33,38 @@ app.get('/', function(req, res){ // when someone connects to server, get request
     }});
 });
 
+/*
+app.post('/register', function(req, res){
 
+    const url = req.url;
+
+    //retrieve data from the form/request 
+
+    var fullname = req.body.fullname;
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password; 
+    var deladdr = req.body.deladdr;
+    var contactnum = req.body.contactnum;
+
+
+    //access the database, then save the data
+
+    //value yung right side
+    var person = {
+        fullname: fullname,
+        username: username,
+        email: email, 
+        password: password,
+        deladdr: deladdr,
+        contactnum: contactnum
+    }
+
+    db.insertOne('registeredusers', person);
+
+});
+
+*/
 app.listen(port, hostname, function() {
     console.log('Server running at: ');
     console.log('http://' + hostname + ':' + port);
