631da22af25c7b3fb4384f5bf812dc838c99c874
Carla Jeannine Cruzada <80302664+enigmaticarly@users.noreply.github.com>
2021-04-29 09:19:44 +0800

    Added route for the separate home page for registered user

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,6 @@ const ctrl = require('../controller/controller.js');
 const registerctrl = require('../controller/registercontroller.js');
 const successctrl = require('../controller/successcontroller.js');
 const acctctrl = require('../controller/acctdetailscontroller.js');
-const searchctrl = require('../controller/searchcontroller.js');
-const homectrl = require('../controller/homecontroller.js');
-const addprodctrl = require('../controller/admin_product_addcontroller.js'); // for ading product
 
 router.get('/', ctrl.getHome);
 router.get('/home', ctrl.getHome);
@@ -13,10 +10,14 @@ router.get('/home', ctrl.getHome);
 router.get('/register', registerctrl.getRegister);
 router.post('/register', registerctrl.postRegister);
 router.get('/getCheckUsername', registerctrl.getCheckUsername);
-
-
-router.get('/acctdetails/:userName', acctctrl.getDetails);
 router.get('/success', successctrl.getSuccess);
+
+router.get('/homeregistered', ctrl.getHomeRegistered);
+
+router.get('/editdetails/:userName', acctctrl.loadDetails);
+router.post('/editdetails', acctctrl.editDetails);
+router.get('/deleteacct', acctctrl.deleteacct);
+router.get('/acctdetails/:userName', acctctrl.getDetails);
 
 router.get('/login', ctrl.getLogin);
 router.get('/acctdetailsnoedit', ctrl.getNoEditAcc);
@@ -26,11 +27,5 @@ router.get('/wishlist', ctrl.getWishlist);
 router.get('/cart', ctrl.getCart);
 router.get('/viewproducts', ctrl.getViewProducts);
 
-router.get('/search', searchctrl.getSearch);
-// router.get('/search', searchctrl.);
-
-router.get('/admin_product_add', addprodctrl.getAdminProdAdd);
-router.post('/admin_product_add', addprodctrl.postAddProd);
-router.get('/getCheckNum', addprodctrl.getCheckNum);
 
 module.exports = router;
