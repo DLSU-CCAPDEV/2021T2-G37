f3529bfa3f00c8d829b05b0ca43153da4473c730
Carla Jeannine Cruzada <80302664+enigmaticarly@users.noreply.github.com>
2021-04-29 10:15:13 +0800

    Add files via upload

--- routes/router.js
+++ routes/router.js
@@ -3,16 +3,33 @@ const ctrl = require('../controller/controller.js');
 const registerctrl = require('../controller/registercontroller.js');
 const successctrl = require('../controller/successcontroller.js');
 const acctctrl = require('../controller/acctdetailscontroller.js');
+const loginctrl = require('../controller/logincontroller.js');
 
 router.get('/', ctrl.getHome);
 router.get('/home', ctrl.getHome);
+
+const addprodctrl = require('../controller/admin_product_addcontroller.js'); // for ading product
+
+router.get('/search', searchctrl.getSearch);
+// router.get('/search', searchctrl.);
+
+router.get('/admin_product_add', addprodctrl.getAdminProdAdd);
+router.post('/admin_product_add', addprodctrl.postAddProd);
+router.get('/getCheckNum', addprodctrl.getCheckNum);
+
+
+router.get('/admin_product_add', addprodctrl.getAdminProdAdd);
+router.post('/admin_product_add', addprodctrl.postAddProd);
+router.get('/getCheckNum', addprodctrl.getCheckNum);
 
 router.get('/register', registerctrl.getRegister);
 router.post('/register', registerctrl.postRegister);
 router.get('/getCheckUsername', registerctrl.getCheckUsername);
 router.get('/success', successctrl.getSuccess);
 
-router.get('/homeregistered', ctrl.getHomeRegistered);
+router.get('/login', loginctrl.getLoginPage);
+router.post('/login', loginctrl.findUser);
+router.get('/loginsuccess', ctrl.getHomeLoggedIn);
 
 router.get('/editdetails/:userName', acctctrl.loadDetails);
 router.post('/editdetails', acctctrl.editDetails);
