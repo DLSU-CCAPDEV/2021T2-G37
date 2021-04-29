54a7931b62600c716100dc06ec499c74243fe34d
tapanna <anna_desembrana@dlsu.edu.ph>
2021-04-29 15:30:25 +0800

    Edited search.css and home.js

--- public/js/home.js
+++ public/js/home.js
@@ -4,3 +4,5 @@ var content = document.getElementById("content");
 function dropdownToggle(){
     document.getElementById('dropmenu').classList.toggle('active'); //clicking the account icon makes it appear, clicking it again makes it disappear.
 }
+
+// display products in page
