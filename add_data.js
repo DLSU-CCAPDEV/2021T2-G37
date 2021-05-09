faefb205b71e30e6e0a75e41b9b6e8cb4f13e581
Carla Jeannine Cruzada <80302664+enigmaticarly@users.noreply.github.com>
2021-05-09 19:29:44 +0800

    Update add_data.js

--- add_data.js
+++ add_data.js
@@ -7,54 +7,66 @@ db.createDatabase();
 
 
 var acc = {
-    fullName: "Tobio Kageyama", 
+    fullName: "Nicole Kageyama", 
     userName: "KingOfTheCourt", 
     email: "kingtobio@gmail.com", 
     pw: "ihatehinata", 
-    deladdr: "Karasuno Volleyball Court", 
-    contactnum: "43109489123"
+    deladdr: "1138 Aguilar Street Sta. Cruz 1000, Manila", 
+    contactnum: "+639178995055"
 };
 
 db.insertOne(collection, acc);
 
 var acc = {
-    fullName: "Toru Oikawa", 
+    fullName: "Carla Oikawa", 
     userName: "bestsetter1", 
     email: "bestsetter@gmail.com", 
     pw: "0nehandt0ss", 
-    deladdr: "Aoba Johsai's Volleyball Court", 
-    contactnum: "528195719385"
+    deladdr: "Unit C D M Building Durian StreetDona Josefa Subdivision Pilar 1740, Las Pinas", 
+    contactnum: "+639165625555"
 };
 
 db.insertOne(collection, acc);
 
 var acc = {
-    fullName: "Makoto Nijima", 
+    fullName: "Anna Nijima", 
     userName: "bestgirl", 
     email: "bestgirl@gmail.com", 
     pw: "FistOfJustic3", 
-    deladdr: "Cafe LeBlanc", 
-    contactnum: "276192847103"
+    deladdr: "2/F Palarca Building Quezon Blvd. Cor. Peredes Street Sampaloc 1000, Cafe LeBlanc", 
+    contactnum: "+639194213030"
 };
 
 db.insertOne(collection, acc);
 var acc = {
-    fullName: "Naoto Shirogane", 
+    fullName: "Clarissa Shirogane", 
     userName: "detectiveprincess", 
     email: "detectiveprincess@gmail.com", 
     pw: "puzzlesRDaBomb", 
-    deladdr: "Inaba", 
-    contactnum: "43109489123"
+    deladdr: "15/F Ps Bank Center777 Paseo De Roxas Street1226, Makati City", 
+    contactnum: "639209614850"
 };
 
 db.insertOne(collection, acc);
 var acc = {
-    fullName: "Phemsee Arisato", 
+    fullName: "Ina Arisato", 
     userName: "bestfemc", 
     email: "darkhour@gmail.com", 
     pw: "door-kun", 
-    deladdr: "Iwatodai Dormitory", 
-    contactnum: "28501927501"
+    deladdr: "3 Anonas Street Potrero 1470, Malabon", 
+    contactnum: "639178856212"
 };
 
 db.insertOne(collection, acc);
+
+var acc = {
+    fullName: "Arya Stark", 
+    userName: "FacelessMan", 
+    email: "girlwithnoface@gmail.com", 
+    pw: "noface", 
+    deladdr: "260 Roosevelt Avenue, San Francisco Del Monte, 1100, Quezon City, Metro Manila", 
+    contactnum: "639176276222"
+};
+
+db.insertOne(collection, acc);
+
