91a450b1d1aaccc2099f6fd726ad6491e069914a
tapanna <80302710+tapanna@users.noreply.github.com>
2021-05-22 17:46:01 +0800

    Added reviews
    - Usernames not yet final
    - Some reviews are repeating

--- add_data.js
+++ add_data.js
@@ -6,6 +6,7 @@ const saltRounds = 10;
 
 const collection = 'User';
 const products = 'Product';
+const reviews = 'Review';
 
 // const User = require('../models/UserSchema.js');
 // const Admin = require('../models/AdminSchema.js');
@@ -244,3 +245,216 @@ var prod = {
 }
 
 db.insertOne(products, prod);
+
+var = {
+    pNum: 100456,
+    userName: "Clarissa",
+    rating: 5.0,
+    rev: "The pants was very smooth and very soft. It was also very light and comfortable. This is probably my best buy ever. I am using it everyday. Planning to buy another one. great job C & S!"
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100456,
+    userName: "Ina",
+    rating: 5.0,
+    rev: "I'm on the heavy side and so wearing jeans is a struggle. But this pair let's me wear jeans again without difficulty (even though I haven't lost any weight)"
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100456,
+    userName: "Nicole",
+    rating: 3.0,
+    rev: "This new version seem to fit tighter than the old version. I like it. However, this new version is mid rise, while the old version is high rise. Would’ve been better if they made this high rise too. EZY usually meant high rise in Uniqlo lingo. Puzzled why they made this mid rise."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100456,
+    userName: "Carla",
+    rating: 4.8,
+    rev: "This is so comfy and stretchable, I like the fit and it is not too skinny fit on me."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100456,
+    userName: "Clar",
+    rating: 5.0,
+    rev: "Great quality jeans! I got this in color blue. These are made to move pants and an all-day-every-day comfort. The material on the inside feels soft against your skin."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100456,
+    userName: "Anna",
+    rating: 4.0,
+    rev: "The pants are not too skinny and indeed give a good amount of stretch. The fit is more on the slim cut side rather than on skin-hugging skinny cut. Elastic waist band is a welcome bonus."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100456,
+    userName: "Patricia",
+    rating: 5.0,
+    rev: "Very much satisfied with my purchase. It fits me very well. Hoping for more shades of blue for this kind of ankle jeans. Thank you so much C & S."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100457,
+    userName: "Nicole",
+    rating: 3.0,
+    rev: "This new version seem to fit tighter than the old version. I like it. However, its fit is not just for me."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100457,
+    userName: "Carla",
+    rating: 4.8,
+    rev: "Only high rise jeans suits me. I'm glad this is one of those jeans."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100457,
+    userName: "Anna",
+    rating: 4.8,
+    rev: "The fit of the pants does not make it hard for the legs to breathe in."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100457,
+    userName: "Anna",
+    rating: 4.8,
+    rev: "The fit of the pants does not make it hard for the legs to breathe in."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100458,
+    userName: "Ina",
+    rating: 5.0,
+    rev: "I'm on the heavy side and so finding a shirt that fits me perfectly is a struggle. But this shirt is a miracle!",
+    img1: "../images/product3a.jpg",
+    img2: "../images/product3b.jpg",
+    img3: ""
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100458,
+    userName: "Nicole", 
+    rating: 3.0,
+    rev: "This new version seem to fit tighter than the old version. I like it. However, the fit of this shirt for me is weird."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100458,
+    userName: "Carla",
+    rating: 4.8,
+    rev: "This is so stylish and comfortable at the same time!"
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100458,
+    userName: "Anna",
+    rating: 4.0,
+    rev: "The shirt embraces my body wonderfully."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100459,
+    userName: "Kent",
+    rating: 5.0,
+    rev: "A bit tight but very smooth. Nice quality socks..",
+    img1: "../images/product4a.jpg".
+    img2: "../images/product4b.jpg"
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100459,
+    userName: "Kyle",
+    rating: 5.0,
+    rev: "This one is comfortable to wear and nice fit on my feet"
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100460,
+    userName: "Juan",
+    rating: 5.0,
+    rev: "If you're looking for something to be part of your capsule wardrobe, this is definitely one of the must have pieces. It's affordable, fit is perfect plus it is extremely soft! I ended buying 3 different colors!",
+    img1: "../images/product5a.jpg".
+    img2: "../images/product5b.jpg"
+}
+
+var = {
+    pNum: 100460,
+    userName: "Dos",
+    rating: 5.0,
+    rev: "It fits well. Nice and comfortable. Store personnel are helpful and accomodating. Affordable!"
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100460,
+    userName: "",
+    rating: ,
+    rev: ""
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100460,
+    userName: "Trish",
+    rating: 4.0,
+    rev: "The texture, the color even the size fits perfectly. I hope there are lots of colors to choose from. Perfect for cold weather as well."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100460,
+    userName: "John",
+    rating: 4.0,
+    rev: "I ordered this shirt last december because i thought that the design was nice. The order arrived and i was right, the shirt was very worth the price. But i wish u can add a larger size. I hope u can add a xxl size because i want to wear this design."
+}
+
+db.insertOne(reviews, rev);
+
+var = {
+    pNum: 100460,
+    userName: "Mary",
+    rating: 5.0,
+    rev: "Hubby loves this.. He has this in 3 colors... Perfect fit... Okay for rounds and clinic..."
+}
+
+db.insertOne(reviews, rev);
+
