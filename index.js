555131defe2f6cdd058c390fdc2b6b03c8fa9f55
tapanna <80302710+tapanna@users.noreply.github.com>
2021-05-24 23:35:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ hostname = process.env.HOSTNAME;
 
 app.use(express.static('public'));
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:<1234>@test-project.h70ee.mongodb.net/clothingandscience?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.use(express.urlencoded({extended: true}));
 
